fdd46e4755c98dc005b1551f8d92207411dc07c7
sourabh <gautamsourabh98@gmail>
2024-05-07 11:30:12 +0530

    change url

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ function Home() {
           <div className="text-1">Hello, my name is</div>
           <div className="text-2">Sourabh Gautam</div>
           <div className="text-3">And I'm a Ui/Ux Developers</div>
-          <a href="https://github.com" target="_blank" rel="noreferrer">Download Resume</a>
+          <a href="https://drive.google.com/file/d/1wniQ7d6Z8CA0Axk5NY7NRv6uhI6gIMg_/view?usp=sharing" target="_blank" rel="noreferrer">Download Resume</a>
         </div>
       </div>
     </section>
